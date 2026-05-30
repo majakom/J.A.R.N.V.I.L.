baf5db3edfb26dfae8aa887fe02abd558006c190
Mikołaj Kiszka <mikolajkiszka123@gmail.com>
2026-05-30 19:34:01 +0200

    Middleware fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
+
+import uvicorn
 from db.seeds.elements_seed import seed_elements
 from db.seeds.yolo_classes_seed import seed_yolo_classes
 from db.session import engine
@@ -22,4 +25,22 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router, prefix="/api")
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
